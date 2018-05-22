def19dbe0de3144d483578a9df15544a75122e76
DLZRR <18643255+DLZRR@users.noreply.github.com>
2018-05-22 16:44:36 +0200

    Add files via upload

---
+++ ALESS_data.py
@@ -0,0 +1,209 @@
+import numpy as np
+from astropy.io import fits
+
+
+#garbage_data = np.loadtxt('ALESS_data_improved.txt', usecols = [0, 46, 47, 53, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 114, 115, 116, 117, 118, 119], skiprows = 1) 
+# 0 id , 53 z_phot, 60 VIMOS starts, first all fluxes, then errors, except 46, 47  # 60-78 Fluxes # errors 79-98 # 99-104 are SPIRE BANDS, again flux, error, .... etc.
+# 114-119 MIPS and PACS BANDS, flux, error, .... etc.
+
+columns_to_use = [61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98]
+
+columns_to_use2 = [46, 47, 114, 115, 116, 117, 118, 119, 99, 100, 101, 102, 103, 104, 30, 31]
+
+hdul = fits.open('sources_aLESS_24jul2012_brightcal_M+S_IRAC_Radio_24+HSO+PACS_zspec.fits')
+
+
+wavelengths = [3675.1, 3429, 3647, 4589, 5377, 6504, 8635, 9502.7, 12275.1, 12304.9, 12530, 16366.01, 21630.58, 21322.79, 21337.8, 35075.1, 44365.8, 56281.0, 75891.6, 232096.0, 684447.6, 979036.1, 1539451.3, 2428217.6, 3408914.0, 4822547.5, 8700000.0]
+
+
+
+#hdul = fits.open(fits_image_filename)
+
+var = hdul[1].data
+#print var[1][-2]
+
+no_redshift = []
+
+array = np.zeros((len(var) + 1, len(wavelengths) * 2 + 2))
+
+for i in range(len(var)):
+
+    array[i+1][0] = float(var[i][0][5:]) # This is the ALESS id
+
+    
+    if var[i][-2] < 0. or var[i][-2] == 99.:
+
+        if var[i][53] < 0. or var[i][53] == 99.:
+
+            if var[i][54] < 0. or var[i][54] == 99.:
+
+                array[i+1][1] = np.random.randint(1,5) # I should remove the source ideally, but I can print the source names later. Not ideal though
+                #print(array[i+1][0])
+                no_redshift.append(i+1)
+                print(array[i+1][0])
+            
+            else:
+
+                array[i+1][1] = var[i][54]
+
+        else:
+    
+            array[i+1][1] = var[i][53]
+
+    else:
+
+        array[i+1][1] = var[i][-2]
+    
+    t = 0
+
+    for j in range(2, 2 * (len(wavelengths) - 8) + 2, 2):
+
+        if np.isnan(var[i][columns_to_use[t]]) or var[i][columns_to_use[t]] == 0.:
+            
+            array[i+1][j] = -99.
+            array[i+1][j+1] = -99.
+
+        elif var[i][columns_to_use[t]] > 100.:
+
+            array[i+1][j] = -99.
+            array[i+1][j+1] = -99.
+
+        elif var[i][columns_to_use[t]] == -99.:
+
+            array[i+1][j] = -99.
+            array[i+1][j+1] = -99.
+            
+        elif var[i][columns_to_use[t+19]] == 0.:
+            
+            array[i+1][j] = 3631. * 10.**(-var[i][columns_to_use[t]]/2.5)
+            array[i+1][j+1] = 3631. * 10.**(-var[i][columns_to_use[t]]/2.5) / 10.
+
+        else:
+
+            # The conversion from magnitude AB to flux in Jansky
+
+            array[i+1][j] = 3631. * 10.**(-var[i][columns_to_use[t]]/2.5)
+            array[i+1][j+1] = var[i][columns_to_use[t + 19]] * 3631./2.5 * np.log(10.) * 10.**(-var[i][columns_to_use[t]]/2.5)
+            
+            if array[i+1][j] == 0. or array[i+1][j]>1.:
+            
+                print(var[i][columns_to_use[t]])
+                print(array[i+1][j+1])
+                
+            if array[i+1][j+1]/array[i+1][j] < 0.1:
+            
+                array[i+1][j+1] = array[i+1][j]/10.
+                
+        #if array[i+1][j] > -98. and array[i+1][j] < 1.e-7:
+            
+        #            print(array[i+1][j])
+        #            print(array[i+1][j+1])
+            # It may very well be possible that they are in micro Jansky, I will try that here. Not true, quite sure it is magnitude AB
+            
+            #array[i+1][j] = var[i][columns_to_use[t]] * 1.e-6
+            #array[i+1][j+1] = var[i][columns_to_use[t + 19]] * 1.e-6
+ 
+        
+        #array[i+1][j+2] = wavelengths[t]
+
+        t += 1
+
+        #print t-1
+
+        #print 'j is', j
+
+    t = 0
+
+    x = 0
+    
+    for s in range(2 * (len(wavelengths) - 8) + 2, 2 * len(wavelengths) + 2, 2):
+        
+        if np.isnan(var[i][columns_to_use2[t]]) or (var[i][columns_to_use2[t]] == 0. and var[i][columns_to_use2[t + 1]] == 0.):
+        
+            array[i+1][s] = -99.
+            array[i+1][s+1] = -99.
+            
+        elif np.isnan(var[i][columns_to_use2[t+1]]):
+            
+            array[i+1][s] = var[i][columns_to_use2[t]] * 1.e-3
+            array[i+1][s+1] = var[i][columns_to_use2[t]] * 1.e-4
+
+        elif var[i][columns_to_use2[t]] == -99.:
+
+            array[i+1][s] = -99.
+            array[i+1][s+1] = -99.
+
+        else:
+
+            array[i+1][s] = var[i][columns_to_use2[t]] * 1.e-3
+            array[i+1][s+1] = var[i][columns_to_use2[t+1]] * 1.e-3 
+
+            '''
+            if x + len(wavelengths) - 8 == 23 or x + len(wavelengths) - 8 == 24 or x + len(wavelengths) - 8 == 25 or x + len(wavelengths) - 8 == 26:
+
+                #print(x + len(wavelengths) - 8)
+                array[i+1][s] = var[i][columns_to_use2[t]] * 1.e-3
+                array[i+1][s+1] = var[i][columns_to_use2[t+1]] * 1.e-3
+
+            # These values should be in flux Jansky
+        
+            elif s != 3 * (len(wavelengths) - 8) + 2:
+
+                array[i+1][s] = 3631. * 10.**(-var[i][columns_to_use2[t]]/2.5)
+                array[i+1][s+1] = var[i][columns_to_use2[t + 1]] * 3631./2.5 * np.log(10.) * 10.**(-var[i][columns_to_use2[t]]/2.5)
+
+                #print array[2][s+1]
+
+                if array[i+1][s+1] <= 0.:
+
+                    array[i+1][s+1] = array[i+1][s] / 10.
+
+            else:
+
+                array[i+1][s] = var[i][columns_to_use2[t]] * 1.e-3
+                array[i+1][s+1] = var[i][columns_to_use2[t+1]] * 1.e-3 
+            '''
+        
+        #array[i+1][s+2] = wavelengths[x + len(wavelengths) - 8]
+
+        #print t
+        
+        if array[i+1][s+1]/array[i+1][s] < 0.1:
+            
+            array[i+1][s+1] = array[i+1][s] / 10.
+        
+        
+        x += 1
+        t += 2
+
+        #print x + len(wavelengths) - 7
+
+        #print 's is', s
+
+    for x in range(2 * (len(wavelengths) - 8) + 2, 2 * len(wavelengths) + 2, 1):
+        
+                if array[i+1][x] > -98. and array[i+1][x] < 1.e-7:
+            
+                    #print(array[i+1][x])
+                    print(1)
+                    #array[i+1][x] = -99.
+                    #print(array[i+1][x])  
+
+#print(array[1])
+#print(array[6])
+#print len(array[0])
+#print np.arange(2, 56, 2).tolist()
+
+
+#for i in range(0, 2*len(wavelengths), 1):
+    
+#    print array[len(var)-3][i], " i is ", i
+
+print(len(no_redshift))
+#print(array[i for i in no_redshift][0])
+tp = tuple(no_redshift)
+array = np.delete(array, tp, axis=0)
+
+    
+
+np.savetxt('ALESS_TRUE_DATA.txt', array, header='ID z VIMOS_U VIMOS_U_error MUSYC_U MUSYC_U_error MUSYC_U38 MUSYC_U38_error MUSYC_B MUSYC_B_error MUSYC_V MUSYC_V_error MUSYC_R MUSYC_R_error MUSYC_I MUSYC_I_error MUSYC_Z MUSYC_Z_error MUSYC_J MUSYC_J_error HAWK_J HAWK_J_error TENIS_J TENIS_J_error MUSYC_H MUSYC_H_error MUSYC_K MUSYC_K_error HAWK_K HAWK_K_error TENIS_K TENIS_K_error IRAC1 IRAC1_error IRAC2 IRAC2_error IRAC3 IRAC3_error IRAC4 IRAC4_error S24 DS24 S70 DS70 S100 DS100 S160 DS160 S250 DS250 S350 DS350 S500 DS500 S870 DS870', comments='')
---
+++ Find_R_bands.py
@@ -0,0 +1,135 @@
+import numpy as np
+import scipy.io
+import matplotlib.pyplot as plt
+
+array = np.zeros((225-33, 34))
+
+filter_wavelengths = [3595, 4640, 6122, 6440, 16620, 35075, 44366, 56281, 75892, 232096, 684448, 1525902, 2428218, 3408914, 4822548, 11991798] # Effective wavelengths from filter website 610 and 20cm need to be added (16500 is h, taken from wikipedia), 119917898 needs to be replaced with a better effective wavelength
+
+
+SDSS = scipy.io.readsav('MULTIWAVE_allfluxes_final.sav')
+
+threshold = 100
+#print SDSS['fr'][0]
+#print SDSS.keys()
+
+#key_names = ['f3', 'f4', 'f5', 'f8', 'f160', 'f70', 'df610', 'df20cm', 'smam', 'zs', 'df350', 'df250', 'dfu', 'dfr', 'epoch', 'dfh', 'f24', 'dfg', 'fr', 'f610', 'df160', 'fu', 'df70', 'rmag', 'f350', 'fg', 'fh', 'mips', 'f500', 'df8', 'df500', 'df5', 'df4', 'f250', 'df3', 'f20cm', 'df24', 'dsmam', 'ew7']
+
+key_names = ['fu', 'dfu', 'fg', 'dfg', 'fr', 'dfr', 'fh', 'dfh', 'f3', 'df3', 'f4', 'df4', 'f5', 'df5', 'f8', 'df8', 'f24', 'df24', 'f70', 'df70', 'f160', 'df160', 'f250', 'df250', 'f350', 'df350', 'f500', 'df500', 'smam', 'dsmam'] # Have not added h band yet drmag does not actually exist, just to make sure program does not whine about this
+
+array = np.zeros((len(SDSS['fg'])+1, len(key_names)+2))
+
+#print SDSS['fu'][0]
+
+#SDSS['fu'] *= 1e-3
+#SDSS['dfu'] *= 1e-3
+#SDSS['fg'] *= 1e-3
+#SDSS['dfg'] *= 1e-3
+#SDSS['fr'] *= 1e-3
+#SDSS['dfr'] *= 1e-3
+#SDSS['fh'] *= 1e-3
+#SDSS['dfh'] *= 1e-3 
+SDSS['f3'] *= 1e-3
+SDSS['df3'] *= 1e-3
+SDSS['f4'] *= 1e-3
+SDSS['df4'] *= 1e-3
+SDSS['f5'] *= 1e-3
+SDSS['df5'] *= 1e-3
+SDSS['f8'] *= 1e-3
+SDSS['df8'] *= 1e-3 
+
+
+for i in range(0, len(SDSS['fg']), 1): #len(SDSS['fg'])
+
+    array[i][0] = SDSS['mips'][i]
+    
+    array[i][1] = SDSS['zs'][i]
+    #print SDSS['mips'][i]
+    #print
+
+    for j in range(2, 10, 2):
+
+        #print j
+
+        #print SDSS[key_names[j-2]][i]
+        
+        
+        if SDSS[key_names[j-2]][i] == 0.:
+
+            array[i][j] = -99.
+            array[i][j+1] = -99.
+
+        elif SDSS[key_names[j-2]][i] > threshold:
+
+            array[i][j] = -99.
+            array[i][j+1] = -99.
+
+            
+        elif SDSS[key_names[j-2]][i] < 1.e-9:
+            
+            array[i][j] = -99.
+            array[i][j+1] = -99.
+
+        else:
+
+            array[i][j] = SDSS[key_names[j-2]][i] 
+            array[i][j+1] = SDSS[key_names[j-2]][i]/10.
+            
+            #array[i+1][8]
+
+            #print array[i+1][j]
+
+    #print array[i+1][j]
+
+    for j in range(10, len(key_names) + 2, 2):
+
+        #print j
+
+        if SDSS[key_names[j-2]][i] == 0. and SDSS[key_names[j-1]][i] == 0.:
+
+            array[i][j] = -99.
+            array[i][j+1] = -99.
+
+        elif SDSS[key_names[j-1]][i] > threshold and SDSS[key_names[j-2]][i] > threshold:
+
+            array[i][j] = -99.
+            array[i][j+1] = -99.
+
+        elif SDSS[key_names[j-2]][i] > threshold and SDSS[key_names[j-1]][i] == 0.:
+
+            array[i][j] = -99.
+            array[i][j+1] = -99.
+
+        elif SDSS[key_names[j-1]][i] > threshold:
+
+            array[i][j] = SDSS[key_names[j-1]][i]
+            array[i][j+1] = SDSS[key_names[j-1]][i] / 10.
+            
+        elif SDSS[key_names[j-2]][i] < 1.e-8 and SDSS[key_names[j-1]][i] < 1.e-9:
+            
+            array[i][j] = -99.
+            array[i][j+1] = -99.
+
+        else:
+
+            array[i][j] = SDSS[key_names[j-2]][i] 
+            array[i][j+1] = SDSS[key_names[j-1]][i]
+            
+            if SDSS[key_names[j-1]][i]/SDSS[key_names[j-2]][i] < 0.1:
+            
+                array[i][j+1] = SDSS[key_names[j-2]][i]/10.
+            
+    for x in range(2, len(key_names) + 2, 2):
+        
+                if array[i][x] > 1000. or array[i+1][x] < 1.e-7:
+            
+                    print(array[i][x+1])
+                    #array[i+1][x] = -99.
+                    #print(array[i+1][x])   
+
+print(array[2])
+print(len(key_names)+2)
+print(np.arange(2,32,2).tolist())
+
+np.savetxt('something_new_new.txt', array, header='ID z fu efu fg efg fr efr fh efh f3 ef3 f4 ef4 f5 ef5 f8 ef8 f24 ef24 f70 ef70 f160 ef160 f250 ef250 f350 ef350 f500 ef500 smam esmam', comments='')
+
